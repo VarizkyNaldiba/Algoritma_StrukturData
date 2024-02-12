2290c17a3a4212cfaec2cfe628aca367177fd995
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2024-02-12 14:50:08 +0700

    Update Pertemuan 1 - Pemilihan

--- Pertemuan 1/LaporanPraktikum/Pemilihan.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Pemilihan {
-    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-        System.out.println("Masukkan Nilai Anda = ");
-    }
-}
