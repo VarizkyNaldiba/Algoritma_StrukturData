9f71f2a914532f8f584727c96cd0a1f92c9ba5c4
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2024-02-14 09:58:03 +0700

    Update Pertemuan 1 - Perulangan

--- Pertemuan 1/Perulangan.java
+++ Pertemuan 1/Perulangan.java
@@ -1,30 +1,29 @@
 import java.util.Scanner;
+
 public class Perulangan {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double NIM;
-
         System.out.print("Masukkan NIM: ");
-        NIM = input.nextDouble();
+        int NIM = input.nextInt(); 
 
-        double duadigit = NIM;
-        double digit = duadigit % 100;
-        System.out.println("n = " + digit);
+        int n = NIM % 100; 
 
-        for (int i = 1; i <= digit; i++) {
-            if (i == 6 && i == 10) {
-                continue;
-            } else {
-                if (i % 2 == 0) {
-                    System.out.print(i);
-                } else {
-                    System.out.print(" * ");
-                }
-            }
-
+        if (n < 10) {
+            n += 10;
         }
 
-        input.close();
+        System.out.println("n = " + n);
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 6 || i == 10) {
+                continue;
+            }
+            if (i % 2 != 0) {
+                System.out.print("*");
+            } else {
+                System.out.print(i);
+            }
+        }
     }
 }
