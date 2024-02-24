0ec840a676da44113daf348575f54e04c9a6e7b9
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2024-02-24 19:58:29 +0700

    Update Pertemuan 2 - Dragon

--- Pertemuan 2/Dragon.java
+++ Pertemuan 2/Dragon.java
@@ -1,28 +1,59 @@
 public class Dragon {
-    int x , y, weidth, height;
-    
-    void moveLeft() {
-    
-    }
-    
-    void moveRight() {
+    int x, y, width = 3, height = 5;
 
+    public Dragon(int horizontal, int vertikal) {
+        x = horizontal;
+        y = vertikal;
+    }
+
+    void moveLeft() {
+        x -= 1;
+        if (x < 0) {
+            detectCollision();
+        }
+    }
+
+    void moveRight() {
+        x += 1;
+        if (x >= width) {
+            detectCollision();
+        }
     }
 
     void moveUp() {
-
+        y -= 1;
+        if (y < 0) {
+            detectCollision();
+        }
     }
 
     void moveDown() {
-
+        y += 1;
+        if (y >= height) {
+            detectCollision();
+        }
     }
 
     void printPosition() {
-
+        System.out.println("Posisi = (" + x + "," + y + ")");
     }
 
-    void detectCollision(int x , int y) {
-
+    void detectCollision() {
+        System.out.println("GAME OVER");
+        System.out.println("HIYAAA SKILL ISSUE");
     }
 
+    public static void main(String[] args) {
+        Dragon gerak = new Dragon(0, 0);
+        gerak.printPosition();
+        gerak.moveRight();
+        gerak.moveRight();
+        gerak.moveLeft();
+        gerak.moveLeft();
+        gerak.moveUp();
+        gerak.moveUp();
+        gerak.moveDown();
+        gerak.moveDown();
+        gerak.printPosition();
+    }
 }
