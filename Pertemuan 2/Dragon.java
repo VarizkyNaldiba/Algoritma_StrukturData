886e18e81b7c2f94295684a3c39e5536505a961b
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2024-02-24 19:09:08 +0700

    Update Pertemuan 2

---
+++ Pertemuan 2/Dragon.java
@@ -0,0 +1,28 @@
+public class Dragon {
+    int x , y, weidth, height;
+    
+    void moveLeft() {
+    
+    }
+    
+    void moveRight() {
+
+    }
+
+    void moveUp() {
+
+    }
+
+    void moveDown() {
+
+    }
+
+    void printPosition() {
+
+    }
+
+    void detectCollision(int x , int y) {
+
+    }
+
+}
