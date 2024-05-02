c046a1cd63040c6a4eedc45e98306232cbced3cf
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2024-05-02 07:34:07 +0700

    update stack

--- Pertemuan9/Barang28.java
+++ Pertemuan9/Barang28.java
@@ -3,7 +3,7 @@ public class Barang28 {
     int kode;
     String nama, kategori;    
 
-    void Barang28(int k, String n, String ktg) {
+    public Barang28(int k, String n, String ktg) {
         this.kode = k;
         this.nama = n;
         this.kategori = ktg;
--- Pertemuan9/Utama28.java
+++ Pertemuan9/Utama28.java
@@ -6,7 +6,56 @@ import java.util.Scanner;
 public class Utama28 {
     public static void main(String[] args) {
         Gudang28 gudang = new Gudang28(7);
-        Scanner sc = new Scanner(System.in);
-        
+        Scanner scanner = new Scanner(System.in);
+
+        // Menu
+        while (true) {
+            System.out.println("Menu");
+            System.out.println("1. Tambah Barang");
+            System.out.println("2. Ambil Barang ");
+            System.out.println("3. Tampilkan tumpukan barang ");
+            System.out.println("4. Keluar");
+            System.out.println("Pilih Operasi : ");
+            int pilihan = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.println(" Masukkan kode Barang : ");
+                    int kode = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Masukkan nama Barang : ");
+                    String nama = scanner.nextLine();
+                    System.out.println("Masukkan nama Kategori : ");
+                    String kategori = scanner.nextLine();
+                    Barang28 barangBaru = new Barang28(kode, nama, kategori);
+                    gudang.tambahBarang(barangBaru);
+                    break;
+                case 2:
+                    System.out.println(" Masukkan kode Barang : ");
+                    kode = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Masukkan nama Barang : ");
+                    nama = scanner.nextLine();
+                    System.out.println("Masukkan nama Kategori : ");
+                    kategori = scanner.nextLine();
+                    barangBaru = new Barang28(kode, nama, kategori);
+                    gudang.ambilBarang();
+                case 3:
+                System.out.println(" Masukkan kode Barang : ");
+                kode = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Masukkan nama Barang : ");
+                nama = scanner.nextLine();
+                System.out.println("Masukkan nama Kategori : ");
+                kategori = scanner.nextLine();
+                barangBaru = new Barang28(kode, nama, kategori);
+                gudang.tampilkanBarang();
+                case 4:
+                    break;
+                default:
+                System.out.println("Pilihan Tidak Valid, Try Again");
+            }
+        }
     }
 }
